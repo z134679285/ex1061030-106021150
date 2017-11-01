dd9ed01ed8ceab63aff25f0b9e74ad1c1e36acbb
z134679285 <32055748+z134679285@users.noreply.github.com>
2017-11-01 21:27:22 +0800

    ex-1061030-106021150
    ex-1061030-106021150

--- src/circle.java
+++ src/circle.java
@@ -6,7 +6,7 @@ public class circle {
         Scanner scn = new Scanner(System.in);
         int X = scn.nextInt();
         int Y = scn.nextInt();
-        if (X<=100 && X>=-100 && Y<=100 && Y>=-100){
+        if (Math.sqrt(X*X+Y*Y)<=100 ){
             System.out.println("inside");
         }else {
             System.out.println("outside");
