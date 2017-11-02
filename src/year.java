77d47fbdf73a46d3b53c8d80173fe9024d822d53
z134679285 <32055748+z134679285@users.noreply.github.com>
2017-11-02 19:31:53 +0800

    ex-1061030-106021150
    ex-1061030-106021150

--- src/year.java
+++ src/year.java
@@ -4,13 +4,27 @@ public class year {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int a = scn.nextInt();
-        if( a % 4 == 0 || a %400 == 0){
-            System.out.println("Bissextile Year");
-        }else if(a % 100 == 0 ){
-            System.out.println("Common Year");
+        int y = scn.nextInt();
+//        if (y % 4 == 0) {
+//            System.out.println("Bissextile Year");
+//        } else if (y % 100 == 0) {
+//            System.out.println("Common Year");
+//        } else if (y % 400 == 0) {
+//            System.out.println("Bissextile Year");
+//        } else {
+//            System.out.println("Common Year");
+//        }
+//    }
+//}
+//}這樣打會判斷錯誤輸入100會優先從上面判斷下來
+            if (y%400==0){
+            System.out.println("Bissextile year");
+        }else if (y%100==0){
+            System.out.println("Common year");
+        }else if (y%4==0){
+            System.out.println("Bissextile year");
         }else{
-            System.out.println("Common Year");
+            System.out.println("Common year");
         }
     }
 }
